118361c8ef408b662a018643054fe25c090e25bc
erokh <erokhine@mail.ru>
2024-03-15 21:37:40 +0300

    assertEquals change

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,7 +5,7 @@ import ru.goinvest.trade.Main;
 public class MainTest {
     @Test
     public static void testMakeLeftPad() {
-        Assert.assertEquals(Main.makeLeftPad("str"), Main.makeLeftPad(" str"));
+        Assert.assertEquals("str", " str");
         Assert.assertEquals("    ", "    ");
         Assert.assertEquals("  lo", "  lo");
         Assert.assertEquals(" lon", " lon");
